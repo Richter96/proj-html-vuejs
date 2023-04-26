ea08fa8c10f6b3f49e86cf0da7110f00854d476f
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-26 13:24:40 +0200

    add structure main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,9 +12,69 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Main</h1>
+    <div class=" container-md debug">
+        <div class="jambotron">
+            <span>jambotron</span>
+        </div>
+        <!-- option of marketing -->
+        <section class="option_Market">
+            <span>Section market</span>
+        </section>
+        <!-- setion Tricks -->
+        <section class="Tricks">
+            <span>Section Tricks</span>
+        </section>
+        <!-- section Popular features -->
+        <section class="Popular_features">
+            <span>Section Popular features</span>
+        </section>
+        <section class="Testimonials">
+            <span>Section Testimonials</span>
+        </section>
+        <section class="latest_posts">
+            <span>Section posts</span>
+        </section>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.debug {
+
+    .jambotron {
+        width: 100%;
+        height: 300px;
+        background-color: lightblue;
+    }
+
+    .option_Market {
+        width: 100%;
+        height: 300px;
+        background-color: antiquewhite;
+    }
+
+    .Tricks {
+        width: 100%;
+        height: 300px;
+        background-color: rgb(168, 98, 8);
+    }
+
+    .Popular_features {
+        width: 100%;
+        height: 300px;
+        background-color: lightcoral;
+    }
+
+    .Testimonials {
+        width: 100%;
+        height: 300px;
+        background-color: lightgray;
+    }
+
+    .latest_posts {
+        width: 100%;
+        height: 300px;
+        background-color: rgb(168, 204, 121);
+    }
+
+}
+</style>
