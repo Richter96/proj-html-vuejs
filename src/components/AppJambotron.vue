e0a45319f93545a5c593985a477555f07154b8dd
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-26 16:30:14 +0200

    add img and taxt at jambotron

---
+++ src/components/AppJambotron.vue
@@ -0,0 +1,49 @@
+<script>
+
+
+export default {
+    name: 'AppJambotron',
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="jambotron d-flex">
+        <div class="text_jambotron">
+            <h1>The biggest platform for your growth.</h1>
+            <p>There are many varlations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                some form</p>
+        </div>
+        <div class="col_60">
+            <img src="../assets/img/508.jpeg" alt="">
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.jambotron {
+    background-color: lightblue;
+
+    .text_jambotron {
+        padding: 4rem;
+    }
+
+    h1 {
+        font-size: 3rem;
+    }
+
+    .col_60 {
+        width: 605;
+    }
+
+
+    img {
+        width: 100%;
+        margin-bottom: -10%;
+    }
+}
+</style>
