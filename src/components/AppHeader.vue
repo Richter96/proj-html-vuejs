c9fc94c5b897d093695f7d22d2e5c7799301c205
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-26 13:12:25 +0200

    start header structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,9 +12,41 @@ export default {
 </script>
 
 <template>
-    <div>
+    <!-- navbar with logo and link  -->
+    <div class="navbarContainer container-md d-flex">
+        <div>
+            logo
+        </div>
+        <div class="d-flex">
+            <ul class="d-flex">
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+            </ul>
+            <button>
+                src
+            </button>
+            <button class="btn btn-primary"> Get Started</button>
+        </div>
 
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.navbarContainer {
+    justify-content: space-around;
+
+    ul {
+        list-style: none;
+
+        li {
+            padding-left: 1rem;
+        }
+    }
+
+
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './assets/scss/App.scss'
 import App from './App.vue'
+import * as bootstrap from 'bootstrap'
 
 
 createApp(App).mount('#app')
