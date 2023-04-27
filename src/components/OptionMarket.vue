947c77fedbcce143c5cda5586abd36853a2630b0
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-27 11:26:59 +0200

    create structure option market

--- src/components/OptionMarket.vue
+++ src/components/OptionMarket.vue
@@ -12,20 +12,102 @@ export default {
 </script>
 
 <template>
-    <div class=" container-md">
-        <div class="col">
-            <h3>Online Marketing</h3>
-            <span>It is a long established fact that a reader will be distracted.</span>
-            <ul>
-                <li>SEO</li>
-                <li>SEM</li>
-                <li>Website Strategi</li>
-                <li>Social management</li>
-            </ul>
+    <div class="container-md optionMarket">
+        <div class="row row-cols-4 d-flex justify-content-center">
+            <div class="col">
+                <div class="card">
+                    <div class="card_img">
+                        <img src="../assets/img/24.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h5>Online Marketing</h5>
+                        <span>It is a long established fact that a reader will be distracted.</span>
+                        <ul>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> SEO</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> SEM</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> Website Strategi</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> Social management</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card_img">
+                        <img src="../assets/img/24.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h5>Online Marketing</h5>
+                        <span>It is a long established fact that a reader will be distracted.</span>
+                        <ul>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> SEO</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> SEM</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> Website Strategi</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> Social management</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card_img">
+                        <img src="../assets/img/24.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h5>Online Marketing</h5>
+                        <span>It is a long established fact that a reader will be distracted.</span>
+                        <ul>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> SEO</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> SEM</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> Website Strategi</li>
+                            <li> <img width="20" src="../assets/img/check.png" alt=""> Social management</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </template>
 
 <style lang="scss">
 @use '../assets/scss/partials/variables' as *;
+
+.optionMarket {
+    margin-top: 4rem;
+    padding-top: 2rem;
+
+    .card {
+        border: 0;
+        padding: 1rem 0rem;
+        flex-direction: row;
+
+        .card_img {
+            img {
+                width: 4rem;
+                padding: 1rem 0;
+            }
+        }
+
+        .card_text {
+            padding: 0.5rem 0rem 0.5rem 2rem;
+
+            ul {
+                padding: 1rem 0;
+                list-style: none;
+
+                li {
+                    line-height: 2.5rem;
+                }
+            }
+        }
+
+
+        span {
+            font-size: 0.8rem;
+            color: $TextGray;
+        }
+    }
+}
 </style>
