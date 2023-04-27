901781ea0d3f3a92d576ffe91044a1605e0bf402
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-27 10:28:13 +0200

    installa fontawson

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import './assets/scss/App.scss'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
 import * as bootstrap from 'bootstrap'
 
+library.add(faMagnifyingGlass, faClock)
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+
