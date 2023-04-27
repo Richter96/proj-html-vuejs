43cf36c39c9445efe178509f583cd2eee20928c6
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-27 11:25:57 +0200

    edit icon font

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { } from '@fortawesome/free-regular-svg-icons'
+
 import * as bootstrap from 'bootstrap'
 
-library.add(faMagnifyingGlass, faClock)
+library.add(faMagnifyingGlass, faCheck)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 
