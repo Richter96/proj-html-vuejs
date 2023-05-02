69a8a071ca8d0edb8d8e0bef74a49d362720b203
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-02 10:32:37 +0200

    add comment and remuv link up to header

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -34,20 +34,28 @@ export default {
 
 <template>
     <footer>
+
+        <!-- FOOTER TOP the map of input for contact teams-->
         <div class="footer_top container_md">
             <div class="row">
                 <div class="contact_title col-12 text-center ">
                     <h3>Contact With our team</h3>
                     <span>If you are planning on developing a product landing.</span>
                 </div>
+
+                <!-- mapp -->
                 <div class="col">
                     <img src="../assets/img/StaticMapService.GetMapIma.png" alt="">
                 </div>
+
+                <!-- info company  -->
                 <div class="col">
                     <img src="../assets/img/35.png" alt="">
                     <span class=" d-block text-black fs-4 py-3"><strong>9840 Share St. Fond Du Lac, WI 54935</strong></span>
                     <span class=" d-block text-black-50 py-1">0 1223 458 789</span>
                     <span class=" d-block text-black-50 ">mali@softbox.com</span>
+
+                    <!-- social zone -->
                     <div class="icones d-flex gap-2 mt-5">
                         <div class="icone border border-1">
                             <a href="#">
@@ -71,7 +79,7 @@ export default {
                         </div>
                     </div>
 
-                    <!-- contact area for mor info  -->
+                    <!-- input area, contact area for mor info  -->
                     <div class="contact my-4">
                         <h3>Get in Touch</h3>
                         <div class="d-flex gap-4 my-4">
@@ -82,11 +90,14 @@ export default {
                             <input type="text" placeholder="Your Subject">
                             <textarea name="" id="" cols="30" rows="5" placeholder="Your Message"></textarea>
                         </div>
-                        <button class="btn btn-primary py-2 my-3">Send Message</button>
+                        <button class="btn btn-primary py-2 my-3 over_black">Send Message</button>
                     </div>
                 </div>
             </div>
         </div>
+
+
+        <!-- FOOTER BOTTOM  links-->
         <div class="footer_bottom border-top">
             <div class="container_md my-5">
                 <div class="row row-cols-4 justify-content-between">
